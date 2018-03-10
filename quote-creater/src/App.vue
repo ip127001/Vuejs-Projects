a6db1f696674c57755a46475a3a1eac93dc213db
Rohit Kumawat <rkumawat125@gmail.com>
2018-03-11 02:57:15 +0530

    already added quotes are shown

--- quote-creater/src/App.vue
+++ quote-creater/src/App.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="container">
-
+      <app-quote-grid :quotes="quotes"></app-quote-grid>
   </div>
 </template>
 
 <script>
-export default {
+  import QuoteGrid from './components/QuoteGrid.vue';
 
-}
+  export default {
+    data: function() {
+      return {
+        quotes: [
+          'hi there it is a quote'
+        ],
+        maxQuotes: 10
+      }
+    },
+    components: {
+      appQuoteGrid: QuoteGrid
+    }
+  }
 </script>
 
 <style>
---
+++ quote-creater/src/components/Quote.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="row">
+    <div class="col-sm-6 col-md-3">
+      <div class="box">
+        <slot></slot>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+.box {
+  padding: 10px;
+  border: 1px solid lightseagreen;
+  box shadow: 0px 3px 3px 0px rgba(0, 0, 0, .19);
+}
+</style>
