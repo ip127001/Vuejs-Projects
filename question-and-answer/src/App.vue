bbfd80a99c1e30a6abdafb91813e75bef4f7bdd5
Rohit Kumawat <rkumawat125@gmail.com>
2018-03-20 16:08:07 +0530

    javascript code added for game

--- question-and-answer/src/App.vue
+++ question-and-answer/src/App.vue
@@ -1,13 +1,46 @@
 <template>
   <div id="app">
-    <h1>hello welcome to question and answer with animation in VueJS</h1>
+    <div class="container">
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+          <h1 class="text-center">question and answer quest</h1>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-lg-6 col-lg-offset-3">
+          <component :is="mode" @answered="answered($event)" @confirmed=" mode = 'app-question' "></component>  
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
+import Question from './components/Question.vue';
+import Answer from './components/Answer.vue';
 
+export default {
+  data() {
+    return {
+          mode: 'app-question'
+    }
+  },
+  methods: {
+    answered(isCorrect) {
+      if(isCorrect) {
+        this.mode = 'app-answer'
+      } else {
+        this.mode = 'app-question';
+        alert('sorry! try again');
+      }
+    }
+  },
+  components: {
+    appQuestion: Question,
+    appAnswer: Answer
+  }
 }
+
 </script>
 
 <style>
---
+++ question-and-answer/src/components/Answer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="alert alert-success text-center">
+      <h1>that's correct</h1>
+      <br>
+      <button class="btn btn-primary" @click="onNextQuestion">Next Question</button>
+  </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            onNextQuestion() {
+                this.$emit('confirmed')
+             }
+        }
+    }
+</script>
